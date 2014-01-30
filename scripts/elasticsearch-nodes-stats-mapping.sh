32c5117be963a1a1d11ac527d939564b1f8d43a9
Mark Walkom <markw@campaignmonitor.com>
2014-01-30 16:41:06 +1100

    Rewritten to use a monthly index and the nodes_stats alias, as doc level TTL isn't overly efficient

--- scripts/elasticsearch-nodes-stats-mapping.sh
+++ scripts/elasticsearch-nodes-stats-mapping.sh
@@ -1,47 +1,61 @@
 #!/bin/bash
 
-# this script creates an index called 'nodes_stats'
-# the index uses 1 shard and has 1 replica (change settings if needed) 
-# the default mapping defines the following:
-# - time to live (ttl) for all documents (default: 60 days)
+# This script creates an index called 'nodes_stats_YYYYMMDD'
+# and then aliases it to nodes_stats.
+# The index uses 8 shards and has 1 replica (change settings if needed).
+# The default mapping defines the following:
 # - fields called '*timestamp' are indexed as dates
 # - string fields are not analyzed
 
-curl -XPUT 'http://localhost:9200/nodes_stats' -d '
+# This originally used document ttl which isn't efficient. Instead
+# the nodes_stats alias is moved monthly and old indexes can then be manually deleted
+
+THISMONTH=`date +%Y%m`
+LASTMONTH=`date --date='last month' +%Y%m`
+
+# Create new month's index
+function createThisMonthsIndex() {
+curl -XPUT "http://localhost:9200/nodes_stats_${THISMONTH}" -d "
 {
-    "settings" : {
-        "number_of_shards" : 1,
-        "number_of_replicas" : 1
+    \"settings\" : {
+        \"number_of_shards\" : 8,
+        \"number_of_replicas\" : 1
     },
-    "mappings" : {
-        "_default_" : {
-            "_all" : {"enabled" : false},
-            "_ttl" : {
-                "enabled" : true,
-                "default" : "60d"
-            },
-            "dynamic_templates" : [
+    \"mappings\" : {
+        \"_default_\" : {
+            \"_all\" : {\"enabled\" : false},
+            \"dynamic_templates\" : [
             {
-                "timestamps_as_date" : {
-                    "match_pattern" : "regex",
-                    "path_match" : ".*timestamp",
-                    "mapping" : {
-                        "type" : "date"
+                \"timestamps_as_date\" : {
+                    \"match_pattern\" : \"regex\",
+                    \"path_match\" : \".*timestamp\",
+                    \"mapping\" : {
+                        \"type\" : \"date\"
                     }
                 }
             },
             {
-                "strings_not_analyzed" : {
-                    "match" : "*",
-                    "match_mapping_type" : "string",
-                    "mapping" : {
-                        "type" : "string",
-                        "index" : "not_analyzed"
+                \"strings_not_analyzed\" : {
+                    \"match\" : \"*\",
+                    \"match_mapping_type\" : \"string\",
+                    \"mapping\" : {
+                       \"type\" : \"string\",
+                       \"index\" : \"not_analyzed\"
                     }
                 }
             }
             ]
         }
     }
-}'
+}"
+}
 
+# Switch the alias to the new month
+function recreateAliases() {
+curl -XDELETE "http://localhost:9200/nodes_stats_${LASTMONTH}/_alias/nodes_stats"
+curl -XPUT "http://localhost:9200/nodes_stats_${THISMONTH}/_alias/nodes_stats"
+}
+
+# Now run
+createThisMonthsIndex
+recreateAliases
